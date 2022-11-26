a26ef69dc32d5fe0d9297a872902df69b3933d3e
abirpuc <mhabirbd2016@gmail.com>
2022-11-27 02:58:46 +0600

    connect with mongo db

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const app = express();
 app.use(express())
 app.use(cors())
 
+require('dotenv').config()
+console.log(process.env.DB_User);
+console.log(process.env.DB_Password)
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://process.env.DB_User:process.env.DB_Password@cluster0.k6rknfb.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 app.get('/', (req,res)=>{
     res.send('used products resale market server is running')
 })

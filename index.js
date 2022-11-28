822559d92222d824b68f212cefd85776ea735d38
abirpuc <mhabirbd2016@gmail.com>
2022-11-28 11:37:19 +0600

    view all products

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000
 
@@ -10,25 +11,42 @@ app.use(express())
 app.use(cors())
 
 require('dotenv').config()
-console.log(process.env.DB_User);
-console.log(process.env.DB_Password)
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://process.env.DB_User:process.env.DB_Password@cluster0.k6rknfb.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@cluster0.k6rknfb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const categoryCollection = client.db('ResaleMarket').collection('productsCategory');
+        const productsCollection = client.db('ResaleMarket').collection('products');
+
+        app.get('/category', async(req, res)=>{
+            const query = {}
+            const result = await categoryCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/products', async(req, res)=>{
+            const query = {}
+            const result = await productsCollection.find().toArray();
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
 
 
-app.get('/', (req,res)=>{
+run().catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => {
     res.send('used products resale market server is running')
 })
 
-app.listen(port, () =>{
+
+app.listen(port, () => {
     console.log(`used products resale market server is running on ${port}`);
 })

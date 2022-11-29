91751190921bf8b1feaf40e7e037464d41e461dc
abirpuc <mhabirbd2016@gmail.com>
2022-11-29 20:02:15 +0600

    booking delete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -55,6 +55,13 @@ async function run() {
             }
             const result = await bookingCollection.find(query).toArray()
             res.send(result)
+        })
+
+        app.delete('/booking/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query= {_id: ObjectId(id)}
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
         })
     }
     finally {

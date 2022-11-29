c85941b01e16045ade7deac2ff814ca7690060a7
abirpuc <mhabirbd2016@gmail.com>
2022-11-29 19:13:06 +0600

    view my booking

--- index.js
+++ index.js
@@ -13,40 +13,51 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@cluster0.k6rknfb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         const categoryCollection = client.db('ResaleMarket').collection('productsCategory');
         const productsCollection = client.db('ResaleMarket').collection('products');
         const bookingCollection = client.db('ResaleMarket').collection('booking');
-    
-        app.get('/products', async(req, res)=>{
+
+        app.get('/products', async (req, res) => {
             const query = {}
             const result = await productsCollection.find().toArray();
             res.send(result)
         })
 
-        app.get('/category', async(req, res)=>{
+        app.get('/category', async (req, res) => {
             const query = {}
             const result = await categoryCollection.find().toArray()
             res.send(result)
         })
 
-        app.get('/products/:category_id', async(req, res)=>{
+        app.get('/products/:category_id', async (req, res) => {
             const id = req.params.category_id;
-            const query = {category_id:id}
+            const query = { category_id: id }
             const result = await productsCollection.find(query).toArray()
             res.send(result)
         })
 
         // booking products
 
-       app.post('/booking', async(req, res) =>{
-        const booking = req.body;
-        const result = await bookingCollection.insertOne(booking);
-        res.send(result)
-       })
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result)
+        })
+
+        app.get('/booking', async(req, res)=>{
+            let query = {}
+            if(req.query.customerEmail){
+                query = {
+                    customerEmail: req.query.customerEmail
+                }
+            }
+            const result = await bookingCollection.find(query).toArray()
+            res.send(result)
+        })
     }
-    finally{
+    finally {
 
     }
 }
